312028f9a69f41b5316e88e887959e1ce3be4659
jiisutti <57663918+jiisutti@users.noreply.github.com>
2024-02-21 13:33:33 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import StyleSheet from './style/style';
+import StyleS from './style/style';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Gameboard from './components/Gameboard';
